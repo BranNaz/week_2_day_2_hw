e8e86c2895d83fbf7c830404d604d808ded33d9f
Brandon Nazario <brandon.nazario21@gmail.com>
2022-07-08 02:15:16 -0400

    week 2 Day 2 hw

---
+++ brandon_module.py
@@ -0,0 +1,13 @@
+import brandon_module
+import math
+from math import pi
+from math import radians
+
+# diam = 23
+diam = input('Enter the diameter of the cirlce:')
+
+def myMath ():
+    circumfrence = math.pi * diam
+    return circumfrence
+
+
---
+++ exercise_3.py
@@ -0,0 +1,7 @@
+l_1 = [1,2,3,4,5,6]
+l_2 = [3,4,5,6,7,8,10]
+
+def l_1():
+    for num in l_1.sort():
+        print(f'New List: {num,sort(l_1)} ')
+l_1()
